e6be5ff64eebc21d3712177899fe09f1718d3397
khiemdc <khiemle.dc@gmail.com>
2020-05-30 21:44:03 -0400

    Updates

---
+++ precache-manifest.c8d5866dff6a725a599dfdf13e784967.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560d50c4a0dfdd44accac527b1bc3342",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "eaf4c4316a5c49abe29d",
+    "url": "/covid19-tracker/static/css/main.0edd6256.chunk.css"
+  },
+  {
+    "revision": "a1326bb6594f22e4509d",
+    "url": "/covid19-tracker/static/js/2.7211a8a9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.7211a8a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf4c4316a5c49abe29d",
+    "url": "/covid19-tracker/static/js/main.b2dad766.chunk.js"
+  },
+  {
+    "revision": "8cd54aa8f9c07f9dab41",
+    "url": "/covid19-tracker/static/js/runtime-main.c8a8ed36.js"
+  },
+  {
+    "revision": "8f09c5ca0ce0f51dd4afc5f081c9f25f",
+    "url": "/covid19-tracker/static/media/coronavirus-3.8f09c5ca.png"
+  }
+]);
