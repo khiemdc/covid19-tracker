d691242ba7b762b05a1639c13b1c57fd57e484ed
khiemdc <khiemle.dc@gmail.com>
2020-05-30 21:28:34 -0400

    tingting first tracker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+import { fetchCountriesData } from './api';
+
+import coronaImage from './images/coronavirus-3.png';
+
+class App extends React.Component {
+    state = {
+        data: {},
+        country: '',
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData});
+
+        // const fetchedCountryData = await fetchCountriesData();
+        // console.log(fetchedCountryData);
+        
+    }
+
+    handleCountryChange = async (country) => {
+        //fetch the data
+        const fetchedCountryData = await fetchCountriesData(country);
+        // console.log(country);
+        // console.log('???', fetchedCountryData);
+        this.setState({ data: fetchedCountryData, country: country });
+        
+        //set the state
+    }
+
+    render() {
+        const { data, country } = this.state;
+        return (
+            <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt="COVID-19" />
+                <Cards data = {data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
+            </div>
+        )
+    }
 }
 
 export default App;
